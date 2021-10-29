ccb9f2db7a2f36ba4e746b28c9a95dc6327381aa
John Hurliman <jhurliman@jhurliman.org>
2021-10-28 17:32:27 -0700

    Add a larger recording for perf testing

--- perf/open.js
+++ perf/open.js
@@ -3,8 +3,10 @@ const { FileReader } = require("../dist/node");
 const path = require("path");
 
 async function main() {
-  const reader = new FileReader(path.join(__dirname, "..", "tests", "sample.ulg"));
-  for (let i = 0; i < 10; i++) {
+  const reader = new FileReader(
+    path.join(__dirname, "..", "tests", "log_6_2021-7-20-11-41-56.ulg"),
+  );
+  for (let i = 0; i < 1; i++) {
     const ulog = new ULog(reader);
     await ulog.open();
   }
