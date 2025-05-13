639fd59aa586f85bd3964cc231502a1bb45a5182
Hans-Joachim Krauch <achim-k@users.noreply.github.com>
2025-05-13 17:24:28 -0300

    Make BlobReader work in nodejs environment (#16)
    ### Changelog
    Make BlobReader work in nodejs environment
    
    ### Docs
    None
    
    ### Description
    The previous `BlobReader` implementation was not fully node compatible.
    The jest tests only worked because the jsdom test environment was
    activated with `/** @jest-environment jsdom */`. This PR uses the
    `Blob.slice().arrayBuffer()` approach, similar to Mcap's
    [BlobReadable](https://github.com/foxglove/mcap/blob/main/typescript/browser/src/BlobReadable.ts)

--- src/web/BlobReader.ts
+++ src/web/BlobReader.ts
@@ -3,48 +3,28 @@ import { Filelike } from "../file";
 // browser reader for Blob|File objects
 export class BlobReader implements Filelike {
   #blob: Blob;
-  #size: number;
 
-  constructor(blob: Blob | File) {
-    if (!(blob instanceof Blob)) {
-      throw new Error("Expected file to be a File or Blob.");
-    }
-
+  public constructor(blob: Blob) {
     this.#blob = blob;
-    this.#size = blob.size;
+  }
+
+  public size(): number {
+    return this.#blob.size;
   }
 
   async open(): Promise<number> {
-    return this.#size;
+    return this.size();
   }
 
   /**
    * Read `length` bytes starting from `offset` bytes.
    */
-  async read(offset: number, length: number): Promise<Uint8Array> {
-    return await new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = function () {
-        reader.onload = null;
-        reader.onerror = null;
-
-        if (reader.result == undefined || !(reader.result instanceof ArrayBuffer)) {
-          reject(new Error("Unsupported format for BlobReader"));
-          return;
-        }
-
-        resolve(new Uint8Array(reader.result));
-      };
-      reader.onerror = function () {
-        reader.onload = null;
-        reader.onerror = null;
-        reject(reader.error ?? new Error("Unknown FileReader error"));
-      };
-      reader.readAsArrayBuffer(this.#blob.slice(offset, offset + length));
-    });
-  }
-
-  size(): number {
-    return this.#size;
+  public async read(offset: number, length: number): Promise<Uint8Array> {
+    if (offset + length > this.size()) {
+      throw new Error(
+        `Read of ${length} bytes at offset ${offset} exceeds blob size ${this.size()}`,
+      );
+    }
+    return new Uint8Array(await this.#blob.slice(offset, offset + length).arrayBuffer());
   }
 }
